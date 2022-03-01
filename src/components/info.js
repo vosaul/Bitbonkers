eb77b5218d6addafb0154d5a52d577c74bcdd261
vosaul <v986963@gmail.com>
2022-03-01 06:59:17 +0200

    info line

---
+++ src/components/info.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Info = () => (
+  <p>
+  <b style={{
+    position: "fixed",
+    top: "0",
+    color: "white",
+    zIndex: "100"
+  }}>
+  Shop our Most Popular Product the 
+  <a href="/products/the-billfodl/">Billfodl
+  </a>! (Free Domestic Shipping)
+  </b>
+  </p>
+)
+export default Info
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,11 @@
 import React from "react"
 import SEO from "../components/seo"
+import Info from "../components/info"
 
-const IndexPage = () => <SEO title="Home" />
-
+const IndexPage = () => (
+<>
+<SEO title="Home" />
+<Info />
+</>
+)
 export default IndexPage
